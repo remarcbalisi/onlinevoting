28246ebfd8bac95e9f9b018455e40e141ac4d32d
kristelahlaine <pabillarankristel@ymail.com>
2015-11-16 16:55:38 +0900

    login and logout feature added

--- wsgi/myproject/system/urls.py
+++ wsgi/myproject/system/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     url(r'^onlinevoting/$', views.index, name='index'),
     url(r'^onlinevoting/register/$', views.user_add, name='user_add'),
+    url(r'^onlinevoting/login/$', views.user_login, name='user_login'),
+    url(r'^onlinevoting/logout/$', views.user_logout, name='user_logout'),
+    url(r'^onlinevoting/home/$', views.user_home, name='user_home'),
 ]
--- wsgi/myproject/system/views.py
+++ wsgi/myproject/system/views.py
@@ -6,33 +6,72 @@ from .models import User
 from django.http import Http404
 
 def index(request):
-	return render(request, 'system/index.html')
+    return render(request, 'system/index.html')
 
 def user_add(request):
+    try:
+        if request.method == 'POST':
+            idnum = request.POST['id_number']
+            fname = request.POST['first_name']
+            lname = request.POST['last_name']
+            contact_number = request.POST['contact_number']
+            email = request.POST['email']
+            password = request.POST['password']
 
-	try:
-		if request.method == 'POST':
+            user = User.objects.create_user(id_number=idnum, password=password)
+            user.email = email
+            user.first_name = fname
+            user.last_name = lname
+            user.contact_number = contact_number
 
-			idnum = request.POST['id_number']
-			fname = request.POST['first_name']
-			lname = request.POST['last_name']
-			contact_number = request.POST['contact_number']
-			email = request.POST['email']
-			password = request.POST['password']
+            return redirect('system.views.user_login')
 
-			user = User.objects.create_user(id_number=idnum, password=password)
-			user.email = email
-			user.first_name = fname
-			user.last_name = lname
-			user.contact_number = contact_number
+        else:
+            form = UserForm()
 
-			return redirect('system.views.user_login')
+    except:
+        form = UserForm()
+        return render(request, 'system/user_add.html', {'form': form})
 
-		else:
-			form = UserForm()
+    return render(request, 'system/user_add.html', {'form': form})
 
-	except:
-		form = UserForm()
-		return render(request, 'system/user_add.html', {'form': form})
+def user_login(request):
 
-	return render(request, 'system/user_add.html', {'form': form})
+    if request.method == 'POST':
+        try:
+            idnum = request.POST['id_number']
+            password = request.POST['password']
+            user = authenticate(id_number=idnum, password=password)
+
+            if user is not None and user.is_active:
+                login(request, user)
+                return redirect('system.views.user_home')
+
+            else:
+                failed = "login failed!"
+                return render(request, 'system/user_login.html', {'failed': failed})
+
+        except User.DoesNotExist:
+            return render(request, 'system/user_login.html')
+
+    elif request.user.is_authenticated():
+        return redirect('system.views.user_home')
+
+    elif not request.user.is_authenticated():
+        return render(request, 'system/user_login.html')
+
+def user_home(request):
+    if request.user.is_authenticated():
+        user = get_object_or_404(User, pk=request.user.pk)
+        return render(request, 'system/user_home.html', {'user': user})
+
+    else:
+        return redirect(request, 'system.views.user_login')	
+
+def user_logout(request):
+    if request.user.is_authenticated():
+        logout(request)
+        return redirect('system.views.user_login')
+
+    else:
+        return redirect('system.views.user_login')

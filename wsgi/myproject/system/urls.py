3a1558e43821f2d167e8c3c8a1dac669d0316c0f
kristelpabillaran <pabillarankristel@ymail.com>
2015-12-09 09:40:55 +0800

    add url 'onlinevoting/view/(?P<pk>[0-9]+)' in urls.py

--- wsgi/myproject/system/urls.py
+++ wsgi/myproject/system/urls.py
@@ -4,12 +4,12 @@ from . import views
 urlpatterns = [
     url(r'^onlinevoting/$', views.user_login, name='user_login'),
     url(r'^onlinevoting/register/$', views.user_add, name='user_add'),
-    url(r'^onlinevoting/home/$', views.user_home, name='user_home'),
     url(r'^onlinevoting/logout/$', views.user_logout, name='user_logout'),
     url(r'^onlinevoting/addposition/$', views.position_add, name='position_add'),
     url(r'^onlinevoting/viewposition/$', views.position_view, name='position_view'),
     url(r'^onlinevoting/addelection/$', views.election_add, name='election_add'),
     url(r'^onlinevoting/addparty/$', views.party_add, name='party_add'),
+    url(r'^onlinevoting/voterhome/$', views.voters_view, name='voters_view'),
     url(r'^onlinevoting/addcandidate/$', views.candidate_add, name='candidate_add'),
     url(r'^onlinevoting/viewcandidate/$', views.candidate_view, name='candidate_view'),
     url(r'^onlinevoting/addcollege/$', views.college_add, name='college_add'),
@@ -21,5 +21,6 @@ urlpatterns = [
     url(r'^onlinevoting/viewcollege/$', views.college_view, name='college_view'),
     url(r'^onlinevoting/college/(?P<pk>[0-9]+)', views.college, name='college'),
     url(r'^onlinevoting/viewbulletin/$', views.bulletin_view, name='bulletin_view'),
-    url(r'^onlinevoting/voterhome/$', views.voters_view, name='voters_view'),
+    url(r'^onlinevoting/home/$', views.user_home, name='user_home'),
+    url(r'^onlinevoting/view/(?P<pk>[0-9]+)', views.candidate, name='candidate'),
 ]

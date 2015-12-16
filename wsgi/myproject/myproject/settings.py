b71c088243913a11166380c0b28fa2f3908185e4
kristelpabillaran <pabillarankristel@ymail.com>
2015-12-17 05:07:54 +0800

    change db name and password

--- wsgi/myproject/myproject/settings.py
+++ wsgi/myproject/myproject/settings.py
@@ -90,9 +90,9 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 DATABASES = {
     'default': {
     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    'NAME': 'onlinevotingdb4',
+    'NAME': 'onlinevotingdb3',
     'USER': 'postgres',
-    'PASSWORD': 'remarc@15',
+    'PASSWORD': 'kristel110496',
     'HOST': 'localhost',
     'PORT': '5432',
     }

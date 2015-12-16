35730980a1f1d6917018ab0517dbabdbb8889308
kristelpabillaran <pabillarankristel@ymail.com>
2015-12-17 07:05:47 +0800

    added deletebulletin url

--- wsgi/myproject/system/urls.py
+++ wsgi/myproject/system/urls.py
@@ -37,4 +37,5 @@ urlpatterns = [
     url(r'^onlinevoting/deletecandidate/(?P<candidate_pk>[0-9]+)/$', views.delete_candidate, name='delete_candidate'),
     url(r'^onlinevoting/deleteparty/(?P<party_pk>[0-9]+)/$', views.delete_party, name='delete_party'),
     url(r'^onlinevoting/updateposition/(?P<position_pk>[0-9]+)/$', views.position_update, name='position_update'),
+    url(r'^onlinevoting/deletebulletin/(?P<bulletin_pk>[0-9]+)/$', views.delete_bulletin, name='delete_bulletin'),
 ]
